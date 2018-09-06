b1853fadca086592d87ccf5213a796b4e4c3b89f
Dan N <dan.noh@gmail.com>
2018-09-06 20:14:45 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -4,4 +4,16 @@ class Person
     @name 
   end 
   
-  def name=
+  def name=(new_name)
+    @name = new_name
+  end 
+  
+  def job
+    @job
+  end 
+  
+  def job=(new_job)
+    @job = new_job
+  end 
+  
+  
